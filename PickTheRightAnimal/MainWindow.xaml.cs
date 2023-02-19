8d954de2f29f4a657b6c97cf84c472fc085b64cc
tenepid <tenepid@gmail.com>
2023-02-19 03:49:00 -0800

    Add project files.

---
+++ PickTheRightAnimal/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PickTheRightAnimal
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            SetUp();
+        }
+
+        private void SetUp()
+        {
+            //throw new NotImplementedException();
+            List<string> emojis = new List<string>()
+            {
+                "🐒", "🐲", "🦛", "🐍",
+                "🐬", "🦈", "🐳", "🕊",
+                "🐧", "🦩", "🐣", "🐘",
+                "🐁", "🦘", "🦥", "🐔"
+            };
+
+            Random random= new Random();
+            matchTextBlock.Text = emojis[random.Next(emojis.Count)];
+
+            foreach(TextBlock textBlock in zaGrid.Children.OfType<TextBlock>())
+            {
+                if (textBlock.Name != "titleTextBlock" && textBlock.Name !="matchTextBlock" )
+                {
+                    int index = random.Next(emojis.Count);
+                    textBlock.Text = emojis[index];
+                    emojis.RemoveAt(index);
+                }
+            }
+        }
+    }
+}
